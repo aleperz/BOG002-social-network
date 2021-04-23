4484998c9954c3f75363510d0b7c5437742e9f6f
Tatiroa994 <tatico994@gmail.com>
2021-04-23 13:19:04 -0500

    input pass component

---
+++ src/components/Input_password.js
@@ -0,0 +1,106 @@
+export class InputPassword extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({
+      mode: "open",
+    });
+  }
+
+  static get observedAttributes() {
+    return ["value"];
+  }
+
+  attributeChangedCallback(atrr, oldValue, newValue) {
+    this[atrr] = newValue;
+    // console.log(newValue, atrr);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  getTemplate() {
+    const template = document.createElement("template");
+    template.innerHTML = `    
+    <label>Contraseña:</label>
+    <div class="container-input">
+        <input type="password" placeholder="Ingrese contraseña" class="input-pass">
+        <div class="eye"></div>
+    </div>
+      
+        
+      
+    ${InputPassword.getStyles()}`;
+    return template;
+  }
+
+  static getStyles() {
+    return `
+    <style>
+      label{
+        display: block;
+        margin-bottom: 5px;
+    }
+    .input-pass{
+        display: block;
+        margin-bottom: 16px;
+        padding: 0.2rem 2rem 0.2rem 0.2rem;
+        box-sizing: border-box;
+        width: 100%; 
+        border-radius: 5px;
+        border: 1px solid #c4c4c4;
+        }
+    input:focus{
+        outline: none;
+        border-color: #4b761f;
+        box-shadow: 0 0 0 0.25rem rgb(161 196 23 /35%);
+    }
+
+    .error{
+      border: solid red 2px;
+      border-radius: 5px;
+    }
+
+    .container-input{
+      position: relative;
+    }
+
+    .eye{
+      position: absolute;
+      top: 2px;
+      right: 4px;
+      background-image: url("./img/visibility_on.svg");
+      background-position: center;
+      background-size: cover;
+      background-repeat: no-repeat;
+      width: 22px;
+      height: 22px;
+      cursor: pointer;
+    }
+
+    .eye.off-eye{
+      background-image: url("./img/visibility_off.svg");
+    }
+
+    </style>`;
+  }
+
+  showpassword() {
+    const btnEyes = this.shadowRoot.querySelector(".eye");
+    const inputPassword = this.shadowRoot.querySelector(".input-pass");
+    btnEyes.addEventListener("click", () => {
+      inputPassword.type = (inputPassword.type === "password") ? "text" : "password";
+      btnEyes.classList.toggle("off-eye");
+    });
+  }
+
+  render() {
+    this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+    const input = this.shadowRoot.querySelector("input");
+    input.addEventListener("change", () => {
+      this.value = input.value;
+      this.showpassword();
+    });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+}
--- src/views_templates/login.js
+++ src/views_templates/login.js
@@ -7,7 +7,7 @@ export const login = `
      <button-action class="secondary" id="btn-google"><img src="./img/google-icon.svg" style="width: 20px; margin-right: 5px;" slot="image"/><span slot="title">Continuar con Google</span></button-action> 
      <hr class="divider"/>
      <input-group type="email" placeholder="Ingrese su correo" id="email-login" class="x"><span slot="title">Correo electronico:</span></input-group>
-     <input-group type="text" placeholder="Ingrese su contraseña" id="pass-login" class="x"><span slot="title">Contraseña:</span></input-group>
+     <input-password id="pass-login"></input-password>
      <label><input type="checkbox"> Recuerdame </label>
      <button-action class="primary" id="btn-login"><span slot="title">Iniciar sesion</span></button-action>  
       <a href="/#register">¿No tienes cuenta? Registrate</a>
