0e13dc0d106a69b2732b1d9ef7cb1f2a173780ad
Tatiroa994 <tatico994@gmail.com>
2021-04-30 12:13:07 -0500

    creando post

--- src/firebase/post_User.js
+++ src/firebase/post_User.js
@@ -1,6 +1,11 @@
 /* eslint-disable class-methods-use-this */
 const db = firebase.firestore();
 export class AdminPost {
+  constructor() {
+    const settings = { timestampsInSnapshots: true };
+    db.settings(settings);
+  }
+
   savePost(description) {
     const user = firebase.auth().currentUser;
     let name;
@@ -12,7 +17,15 @@ export class AdminPost {
       uid = user.uid;
     }
     return db.collection("posts").doc().set({
-      description, name, photoUrl, uid,
+      description,
+      name,
+      photoUrl,
+      uid,
+      date: Date.now(),
     });
+  }
+
+  getPost(callback) {
+    return db.collection("posts").onSnapshot(callback);
   }
 }
--- src/views_templates/timeline.js
+++ src/views_templates/timeline.js
@@ -2,6 +2,9 @@ export const timeline = `
 <section class="section-timeline">
 <header-general></header-general>
 <modal-post id="modal-post"><span slot="title">Crear publicacion</span></modal-post>
+<div id="container-post">
+<data-post></data-post>
+</div>
 <open-modal></open-modal>
 <bar-navegation id="bar-navegacion"></bar-navegation>
 </section>
