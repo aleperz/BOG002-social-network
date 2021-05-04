2916013c1b52b994346b9639140029763741387b
aleperz <alejandraperez.659@gmail.com>
2021-05-03 21:31:43 -0500

    edicion de post1

---
+++ src/components/edit_delete_post.js
@@ -0,0 +1,79 @@
+export class EditDeletePost extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({
+      mode: "open",
+    });
+  }
+
+  static getTemplate() {
+    const template = document.createElement("template");
+    template.innerHTML = `    
+    <div class="containerMenu">
+      <div>
+        <span>...</span>
+      </div>
+      <div class="optionsHide">
+        <p id="edit">Editar</p>
+        <p id="delete">Borrar</p>
+      </div>    
+    </div>
+        ${EditDeletePost.getStyles()}`;
+    return template;
+  }
+
+  static getStyles() {
+    return `
+        <style>
+      .containerMenu{
+        position: absolute;
+        right: 12px;
+        top: -16px;
+        cursor:pointer;
+    }
+    .containerMenu span{
+      font-size: 2.5rem;
+    }
+      
+      .optionsHide.visible{
+        display:block;
+        width: 100px;
+        position: absolute;
+        text-align: center;
+        right: -10px;
+        background-color: #d2d2d2;
+        border-radius: 3px;
+      }
+      .optionsHide{
+        display:none
+      }
+      .optionsHide p{
+        width:100%;
+        margin: 0;
+        padding: 10px 0;
+        border-radius: 3px;
+      } 
+      .optionsHide p:hover{
+        background-color:gray;
+        border-radius:3px;
+        color:white;
+      }
+
+         </style>`;
+  }
+
+  render() {
+    this.shadowRoot.appendChild(
+      EditDeletePost.getTemplate().content.cloneNode(true)
+    );
+    const menu = this.shadowRoot.querySelector("span");
+    const optionMenu = this.shadowRoot.querySelector(".optionsHide");
+    menu.addEventListener("click", () => {
+      optionMenu.classList.toggle("visible");
+    });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+}
